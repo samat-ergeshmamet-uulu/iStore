797eeae0aeb5f45fb2ce2627505ec77ec0675d24
Samat Ergeshmamet uulu <ergeshmametuulusamat@gmail.com>
2021-12-30 05:45:24 +0300

    Added common configurations to start a project, created html, css and script files

---
+++ script.js
@@ -0,0 +1,17 @@
+/* <!-- Section 1 (Showcase) -->
+<!-- End Section 1 (Showcase) --> */
+
+/* <!-- Section 2 (iPhone) -->
+<!-- End Section 2 (iPhone) --> */
+
+/* <!-- Section 3 (Macbook Air) -->
+<!-- End Section 3 (Macbook Air) --> */
+
+/* <!-- Section 4 (Apple watch) -->
+<!-- End Section 4 (Apple watch) --> */
+
+/* <!-- Section 5 (AirPods) -->
+<!-- End Section 5 (Airpods) --> */
+
+/* <!-- Section 6 (Contact Us) -->
+<!-- End Section 6 (Contact Us) --> */
