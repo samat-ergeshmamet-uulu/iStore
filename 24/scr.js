7623004333f9ccfa6a51c36f1fc231d9de93182e
Zhyldyz <zsultanova03@gmail.com>
2022-01-12 14:23:30 +0100

    file added

---
+++ 24/scr.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector("recipe-btn");
+
+var info = document.querySelector(".info");
+
+ function openModal() {
+     info.style.display = "block";
+ }
+
+ function closeModal() {
+     info.style.display = "none";
+ }
