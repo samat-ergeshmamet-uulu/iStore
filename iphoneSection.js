dd3521d027f491c641c0c33d41abb6f8e50b67b1
”Phoenixgit config --global user.email psummerchallenge@gmail.comgit config user.name Phoenixgit config --global user.email psummerchallenge@gmail.comgit config --global user.email psummerchallenge@gmail.comgit config user.email psummerchallenge@gmail.com <aikawanmei@gmail.com>
2022-01-12 17:30:44 -1200

    add iphone section

---
+++ iphoneSection.js
@@ -0,0 +1,12 @@
+function change1(){
+  document.getElementById("12ProMax").src="./img/12-Pro-Max-Silver.webp";
+ }
+function change2(){
+  document.getElementById("12ProMax").src="./img/12-Pro-Max-Graphite.webp";
+}
+function change3(){
+  document.getElementById("12ProMax").src="./img/12-Pro-Max-Gold.webp";
+}
+function change4(){
+  document.getElementById("12ProMax").src="./img/12-Pro-Max-Sierra-Blue.webp";
+}
