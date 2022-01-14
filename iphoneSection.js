869a2de67123ff8a22712a5d506716db80f08133
”Phoenixgit config --global user.email psummerchallenge@gmail.comgit config user.name Phoenixgit config --global user.email psummerchallenge@gmail.comgit config --global user.email psummerchallenge@gmail.comgit config user.email psummerchallenge@gmail.com <aikawanmei@gmail.com>
2022-01-14 11:25:16 -1200

    edited iphone part

--- iphoneSection.js
+++ iphoneSection.js
@@ -10,3 +10,22 @@ function change3(){
 function change4(){
   document.getElementById("12ProMax").src="./img/12-Pro-Max-Sierra-Blue.webp";
 }
+
+let liSixFour = document.querySelector('#sixFour');
+let lioneTwoEight = document.querySelector('#oneTwoEight');
+let litwoFiveSix = document.querySelector('#twoFiveSix');
+let lifiveOneTwo = document.querySelector('#fiveOneTwo');
+let priceP = document.querySelector('#ichoose__price-inner');
+
+liSixFour.addEventListener('click', ()=>{
+  priceP.innerText = '$1350.00'
+});
+lioneTwoEight.addEventListener('click', ()=>{
+  priceP.innerText = '$1470.00'
+});
+litwoFiveSix.addEventListener('click', ()=>{
+  priceP.innerText = '$1530.00'
+});
+lifiveOneTwo.addEventListener('click', ()=>{
+  priceP.innerText = '$1620.00'
+});
